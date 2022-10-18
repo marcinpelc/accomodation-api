13e2191e7e8e62d48085b3de452e1b6c5d53d2a5
Twoje Imię Nazwisko <toja@example.com>
2022-10-18 08:20:21 +0200

    Add application and inbound modules

---
+++ application/src/main/java/mpelc/example/accommodation/AccommodationApplication.java
@@ -0,0 +1,11 @@
+package mpelc.example.accommodation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccommodationApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(AccommodationApplication.class, args);
+  }
+}
